148b8e536f0afdfc599184aecefc2546d6d8092a
jerrx18 <happyjerrychristmas@gmail.com>
2023-02-10 23:14:24 +0900

    commit

---
+++ Week3_5/src/A.java
@@ -0,0 +1,9 @@
+public class A {
+	public A() {
+		System.out.println("생성자 A");
+	}
+	
+	public A(int x) {
+		System.out.println("매개변수 생성자 A");
+	}
+}
---
+++ Week3_5/src/Week3_5.java
@@ -0,0 +1,8 @@
+public class Week3_5 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		B b = new B();
+		B b2 = new B(5);
+	}
+}
